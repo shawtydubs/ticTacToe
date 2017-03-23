231a33c4adaafa911e3a836c9f4d91c2ce89a65a
Claire Walters <claire.walters@opower.com>
2017-03-23 19:15:45 -0400

    add functionality to end game on win or draw

--- game.js
+++ game.js
@@ -1,24 +1,72 @@
+var gameOver = false;
+
 var playerToken = 'X';
 var computerToken = 'O';
 
+var rows = [
+    'top',
+    'middle',
+    'bottom',
+    'left',
+    'center',
+    'right',
+    'diag-one',
+    'diag-two'
+]
+
 $('.square').click(function() {
-    $(this).addClass('taken');
+    $(this).addClass('taken').addClass('player');
     $(this).append(playerToken);
     checkFinished();
-    computerTurn();
+    if (!gameOver) computerTurn();
 });
 
 function computerTurn() {
     var openSquares = $('.square').filter(':not(.taken)');
     var selectedSquare = openSquares[Math.floor(Math.random() * openSquares.length)];
-    $(selectedSquare).addClass('taken');
+    $(selectedSquare).addClass('taken').addClass('computer');
     $(selectedSquare).append(computerToken);
     checkFinished();
 }
 
 function checkFinished() {
-    var openSquares = $('.square').filter(':not(.taken)');
-    if (openSquares.length == 0) {
-        $('#endGame').removeClass('hidden');
+    var playerSquares = $('.square').filter('.player')
+    var computerSquares = $('.square').filter('.computer');
+    var takenSquares = $('.square').filter('.taken');
+    
+    var playerCount = addSquares(playerSquares);
+    var computerCount = addSquares(computerSquares);
+
+    console.group('Player Count');console.log(playerCount);console.groupEnd();
+    console.group('Computer Count');console.log(computerCount);console.groupEnd();
+
+    if (Object.values(playerCount).indexOf(3) > -1) {
+        $('#endGame').append('You won!').removeClass('hidden');
+        endGame();
+    } 
+    else if (Object.values(computerCount).indexOf(3) > -1) {
+        $('#endGame').append('Computer won!').removeClass('hidden');
+        endGame();
+    } 
+    else if (takenSquares.length == 9) {
+        $('#endGame').append("It's a draw").removeClass('hidden');
+        endGame();
     }
+}
+
+function addSquares(squares) {
+    var squareCount = {};
+    for (c = 0; c < rows.length; c++) {
+        for (s = 0; s < squares.length; s++) {
+            if ($(squares[s]).hasClass(rows[c])) {
+                squareCount[rows[c]] = squareCount[rows[c]] + 1 || 1;
+            }
+        }
+    }
+    return squareCount;
+}
+
+function endGame() {
+    gameOver = true;
+    $('.square').addClass('taken');
 }

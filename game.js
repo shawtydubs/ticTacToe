fc425199d64e365dead6fbd92a5d5df46995c305
Claire Walters <claire.walters@opower.com>
2017-03-23 18:13:46 -0400

    add end game detection

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ var computerToken = 'O';
 $('.square').click(function() {
     $(this).addClass('taken');
     $(this).append(playerToken);
+    checkFinished();
     computerTurn();
 });
 
@@ -12,6 +13,12 @@ function computerTurn() {
     var selectedSquare = openSquares[Math.floor(Math.random() * openSquares.length)];
     $(selectedSquare).addClass('taken');
     $(selectedSquare).append(computerToken);
+    checkFinished();
+}
 
-    console.log(selectedSquare);
+function checkFinished() {
+    var openSquares = $('.square').filter(':not(.taken)');
+    if (openSquares.length == 0) {
+        $('#endGame').removeClass('hidden');
+    }
 }
